6e1f2596d14b2218f3c4a8f2dc0d72929da0cdde
Shahar <lshahar@users.noreply.github.com>
2020-06-19 17:48:54 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -34,6 +34,9 @@ IGNORE=(
   '/var/lib/update_engine/prefs'
   '/var/log'
   '/log/journal'
+  '/host-fs/var/lib/lxcfs'
+  '/host-fs/var/cache/apt'
+  '/host-fs/var/lib/apt/lists/'
   )
 
 LOGDIR=/logs
